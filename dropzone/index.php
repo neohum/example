e763ccf9b56f4ac14e912b31064645c0004f7081
민정일 <neohum77@gmail.com>
2024-08-07 12:05:56 +0900

    dev/20240807_001:example

---
+++ dropzone/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+<!-- Plugin -->
+<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+  <body>
+
+  <form action="upload.php" class="dropzone"></form>
+  </body>
+</html>
